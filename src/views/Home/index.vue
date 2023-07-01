37aa84d928d27ddaa87d61e3aa296643d7b31435
胥天昊 <1499487526@qq.com>
2023-07-01 16:59:24 +0800

    m

---
+++ src/views/Home/index.vue
@@ -0,0 +1,109 @@
+<template>
+  <v-chart class="chart" :option="option" autoresize />
+</template>
+
+<script>
+import VChart from "vue-echarts";
+import { ref, defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HelloWorld",
+  components: {
+    VChart,
+  },
+
+  setup() {
+    const option = ref({
+      title: {
+        text: "您最近的提交工程量",
+        subtext: "Fake Data",
+        left: "center",
+      },
+
+      legend: {
+        left: "center",
+        top: "bottom",
+        data: [
+          "rose1",
+          "rose2",
+          "rose3",
+          "rose4",
+          "rose5",
+          "rose6",
+          "rose7",
+          "rose8",
+        ],
+      },
+      toolbox: {
+        show: true,
+        feature: {
+          mark: { show: true },
+          dataView: { show: true, readOnly: false },
+          restore: { show: true },
+          saveAsImage: { show: true },
+        },
+      },
+      series: [
+        // 第一个图片
+        {
+          name: "Radius Mode",
+          type: "pie",
+          radius: [20, 140],
+          center: ["25%", "50%"],
+          roseType: "radius",
+          itemStyle: {
+            borderRadius: 5,
+          },
+          label: {
+            show: false,
+          },
+          emphasis: {
+            label: {
+              show: true,
+            },
+          },
+          data: [
+            { value: 40, name: "rose 1" },
+            { value: 33, name: "rose 2" },
+            { value: 28, name: "rose 3" },
+            { value: 22, name: "rose 4" },
+            { value: 20, name: "rose 5" },
+            { value: 15, name: "rose 6" },
+            { value: 12, name: "rose 7" },
+            { value: 10, name: "rose 8" },
+          ],
+        },
+        // 第二个
+        {
+          name: "Area Mode",
+          type: "pie",
+          radius: [20, 140],
+          center: ["75%", "50%"],
+          roseType: "area",
+          itemStyle: {
+            borderRadius: 5,
+          },
+          data: [
+            { value: 30, name: "rose 1" },
+            { value: 28, name: "rose 2" },
+            { value: 26, name: "rose 3" },
+            { value: 24, name: "rose 4" },
+            { value: 22, name: "rose 5" },
+            { value: 20, name: "rose 6" },
+            { value: 18, name: "rose 7" },
+            { value: 16, name: "rose 8" },
+          ],
+        },
+      ],
+    });
+
+    return { option };
+  },
+});
+</script>
+
+<style scoped>
+.chart {
+  height: 100vh;
+}
+</style>
---
+++ src/views/Layout/index.vue
@@ -0,0 +1,112 @@
+<template>
+  <div>
+    <el-container>
+      <el-aside>
+        <el-menu
+          default-active="2"
+          class="el-menu-vertical-demo"
+          @open="handleOpen"
+          @close="handleClose"
+          style="height: 852px"
+        >
+          <el-menu-item index="1" @click="toHome">
+            <i class="el-icon-menu"></i>
+            <span slot="title">首页</span>
+          </el-menu-item>
+
+          <el-menu-item index="2" @click="toListExInformation">
+            <i class="el-icon-document"></i>
+            <span slot="title">查看异常信息</span>
+          </el-menu-item>
+
+          <el-menu-item index="3" @click="toListTesting">
+            <i class="el-icon-setting"></i>
+            <span slot="title">查看检测数据</span>
+          </el-menu-item>
+          <el-menu-item index="4" @click="toListAssign">
+            <i class="el-icon-location"></i>
+            <span slot="title">查看委派信息</span>
+          </el-menu-item>
+        </el-menu>
+      </el-aside>
+
+      <el-container>
+        <el-header style="text-align: right; font-size: 16px">
+          <el-dropdown>
+            <i class="el-icon-setting" style="margin-right: 15px"></i>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>查看</el-dropdown-item>
+              <el-dropdown-item>新增</el-dropdown-item>
+              <el-dropdown-item>删除</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+          <span>管理员名字</span>
+        </el-header>
+        <!-- 一个分割线 -->
+        <el-divider></el-divider>
+        <el-main>
+          <!-- main主体 -->
+          <RouterView />
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    toHome() {
+      this.$router.push("/").catch((error) => {
+        if (error.name !== "NavigationDuplicated") {
+          throw error;
+        } else {
+          console.log("Navigation duplicated");
+        }
+      });
+    },
+    toListExInformation() {
+      console.log("hhhh");
+      this.$router.push("/ListExInformation").catch((error) => {
+        if (error.name !== "NavigationDuplicated") {
+          throw error;
+        } else {
+          console.log("Navigation duplicated");
+        }
+      });
+    },
+    toListTesting() {
+      this.$router.push("/ListTesting").catch((error) => {
+        if (error.name !== "NavigationDuplicated") {
+          throw error;
+        } else {
+          console.log("Navigation duplicated");
+        }
+      });
+    },
+    toListAssign() {
+      this.$router.push("/ListAssign").catch((error) => {
+        if (error.name !== "NavigationDuplicated") {
+          throw error;
+        } else {
+          console.log("Navigation duplicated");
+        }
+      });
+    },
+
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    },
+  },
+};
+</script>
+<style>
+.el-header {
+  background-color: #ffffff;
+  color: #333;
+  line-height: 60px;
+}
+</style>

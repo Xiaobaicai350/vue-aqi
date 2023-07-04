78d5e0818564dc98433ba0805c0852e1c310d855
胥天昊 <1499487526@qq.com>
2023-07-04 11:30:49 +0800

    m

--- src/views/AllDo/index.vue
+++ src/views/AllDo/index.vue
@@ -1,15 +1,18 @@
 <template>
   <el-table :data="tableData" style="width: 100%">
     <el-table-column prop="id" label="id"> </el-table-column>
-    <el-table-column prop="description" label="description"> </el-table-column>
-    <el-table-column prop="address" label="address"> </el-table-column>
-    <el-table-column prop="vAQILevel" label="vAQILevel"> </el-table-column>
+    <el-table-column prop="exMessage.description" label="description">
+    </el-table-column>
+    <el-table-column prop="exMessage.address" label="address">
+    </el-table-column>
+    <el-table-column prop="exMessage.vAQILevel" label="vAQILevel">
+    </el-table-column>
     <el-table-column prop="publicName" label="举报人姓名"> </el-table-column>
     <el-table-column prop="updateTime" label="updateTime"> </el-table-column>
     <el-table-column prop="SO2" label="SO2"> </el-table-column>
     <el-table-column prop="PM" label="PM2.5"> </el-table-column>
     <el-table-column prop="CO" label="updateTime"> </el-table-column>
-    <el-table-column prop="AQI" label="AQI"> </el-table-column>
+    <el-table-column prop="aqilevel" label="aqilevel"> </el-table-column>
   </el-table>
 </template>
 
